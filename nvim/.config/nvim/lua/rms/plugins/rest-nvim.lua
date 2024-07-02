b9a42ef112d2d18f309b0567fd917c2fdfc090ec
Robbi <30313644+rollsrobby@users.noreply.github.com>
2024-07-02 06:47:10 +0200

    disable rest client

--- nvim/.config/nvim/lua/rms/plugins/rest-nvim.lua
+++ nvim/.config/nvim/lua/rms/plugins/rest-nvim.lua
@@ -1,32 +1,32 @@
 return {
-	{
-		"vhyrro/luarocks.nvim",
-		priority = 1000,
-		config = true,
-		opts = {
-			rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
-		}
-	},
-	{
-		"rest-nvim/rest.nvim",
-		ft = "http",
-		dependencies = { "luarocks.nvim" },
-		config = function()
-			require("rest-nvim").setup({
-				result = {
-					split = {
-						in_place = true,
-					},
-				},
-				keybinds = {
-					{
-						"<leader>rr", "<cmd>Rest run<cr>", "Run request under the cursor",
-					},
-					{
-						"<leader>rl", "<cmd>Rest run last<cr>", "Re-run latest request",
-					},
-				}
-			})
-		end,
-	}
+	-- {
+	-- 	"vhyrro/luarocks.nvim",
+	-- 	priority = 1000,
+	-- 	config = true,
+	-- 	opts = {
+	-- 		rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
+	-- 	}
+	-- },
+	-- {
+	-- 	"rest-nvim/rest.nvim",
+	-- 	ft = "http",
+	-- 	dependencies = { "luarocks.nvim" },
+	-- 	config = function()
+	-- 		require("rest-nvim").setup({
+	-- 			result = {
+	-- 				split = {
+	-- 					in_place = true,
+	-- 				},
+	-- 			},
+	-- 			keybinds = {
+	-- 				{
+	-- 					"<leader>rr", "<cmd>Rest run<cr>", "Run request under the cursor",
+	-- 				},
+	-- 				{
+	-- 					"<leader>rl", "<cmd>Rest run last<cr>", "Re-run latest request",
+	-- 				},
+	-- 			}
+	-- 		})
+	-- 	end,
+	-- }
 }
