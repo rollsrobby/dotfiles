655dc275ac645facd2957332a28ab240433dee30
Robbi <30313644+rollsrobby@users.noreply.github.com>
2024-07-16 18:34:35 +0200

    disable prettier auto formatting

--- nvim/.config/nvim/lua/rms/plugins/prettier.lua
+++ nvim/.config/nvim/lua/rms/plugins/prettier.lua
@@ -2,12 +2,14 @@ return {
 	'prettier/vim-prettier',
 	build = 'pnpm install -g prettier',
 	config = function()
-		vim.api.nvim_create_augroup('BufWritePreFormatter', {})
-		vim.api.nvim_create_autocmd('BufWritePre',
-			{
-				command = 'Prettier',
-				pattern = '*.js,*.jsx,*.ts,*.tsx,*.css,*.scss,*.json,*.graphql,*.md',
-				group = 'BufWritePreFormatter'
-			})
+		-- vim.g['prettier#autoformat_config_present'] = 1
+		-- vim.g['prettier#config#config_precedence'] = 'prefer-file'
+		-- vim.api.nvim_create_augroup('BufWritePreFormatter', {})
+		-- vim.api.nvim_create_autocmd('BufWritePre',
+		-- 	{
+		-- 		command = 'Prettier',
+		-- 		pattern = '*.js,*.jsx,*.ts,*.tsx,*.css,*.scss,*.json,*.graphql,*.md',
+		-- 		group = 'BufWritePreFormatter'
+		-- 	})
 	end
 }
