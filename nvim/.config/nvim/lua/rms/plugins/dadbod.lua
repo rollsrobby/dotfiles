78a20f622a0850896129618daffc43ec8898d377
Robbi <30313644+rollsrobby@users.noreply.github.com>
2024-08-21 21:38:22 +0200

    added cmds

--- nvim/.config/nvim/lua/rms/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/rms/plugins/dadbod.lua
@@ -1,13 +1,19 @@
 return {
-'kristijanhusak/vim-dadbod-ui',
+  'kristijanhusak/vim-dadbod-ui',
   dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
+    { 'tpope/vim-dadbod',                     lazy = true },
     { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
-	},
-	init = function ()
-		vim.g.db_ui_use_nerd_fonts = 1
-	end,
-	keys = {
-		{ '<leader>du', ':DBUIToggle<cr>', desc = 'Toggle DBUI' },
-	},
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+  keys = {
+    { '<leader>du', ':DBUIToggle<cr>', desc = 'Toggle DBUI' },
+  },
 }
