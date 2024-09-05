332ab956e1d88d69d6099d19e611eb2490ffa21e
Robbi <30313644+rollsrobby@users.noreply.github.com>
2024-09-05 21:04:39 +0200

    update rest config

--- nvim/.config/nvim/lua/rms/plugins/rest-nvim.lua
+++ nvim/.config/nvim/lua/rms/plugins/rest-nvim.lua
@@ -1,32 +1,17 @@
 return {
-	-- {
-	-- 	"vhyrro/luarocks.nvim",
-	-- 	priority = 1000,
-	-- 	config = true,
-	-- 	opts = {
-	-- 		rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
-	-- 	}
-	-- },
-	-- {
-	-- 	"rest-nvim/rest.nvim",
-	-- 	ft = "http",
-	-- 	dependencies = { "luarocks.nvim" },
-	-- 	config = function()
-	-- 		require("rest-nvim").setup({
-	-- 			result = {
-	-- 				split = {
-	-- 					in_place = true,
-	-- 				},
-	-- 			},
-	-- 			keybinds = {
-	-- 				{
-	-- 					"<leader>rr", "<cmd>Rest run<cr>", "Run request under the cursor",
-	-- 				},
-	-- 				{
-	-- 					"<leader>rl", "<cmd>Rest run last<cr>", "Re-run latest request",
-	-- 				},
-	-- 			}
-	-- 		})
-	-- 	end,
-	-- }
+  {
+    "rest-nvim/rest.nvim",
+    config = function()
+      require("rest-nvim").setup({
+        ui = {
+          keybinds = {
+            prev = "P",
+            next = "N",
+          }
+        },
+        vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<cr>", { desc = "Run request under the cursor" }),
+        vim.keymap.set("n", "<leader>rl", "<cmd>Rest last<cr>", { desc = "Re-run last request" }),
+      })
+    end,
+  }
 }
