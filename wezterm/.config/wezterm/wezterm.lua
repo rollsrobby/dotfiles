756d920e151a497e0266e942a2acc4836fc865c3
Robbi <30313644+rollsrobby@users.noreply.github.com>
2024-09-12 19:30:46 +0200

    use gruvbox theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,34 +1,29 @@
 local wezterm = require 'wezterm'
 return {
-	color_scheme = 'Catppuccin Mocha',
-	colors = {
-		background = '#1d2021';
-	},
-	enable_tab_bar = false,
-	font_size = 16.0,
-	macos_window_background_blur = 30,
-	-- window_background_opacity = 0.9,
-	window_decorations = 'RESIZE',
-	window_close_confirmation = 'NeverPrompt',
-	-- window_padding = {
-	-- 	left = 0,
-	-- 	right = 0,
-	-- 	top = 0,
-	-- 	bottom = 0,
-	-- },
-	native_macos_fullscreen_mode = true,
-	keys = {
-		{
-			key = 'n',
-			mods = 'SHIFT|CTRL',
-			action = wezterm.action.ToggleFullScreen,
-		},
-	},
-	mouse_bindings = {
-	  {
-	    event = { Up = { streak = 1, button = 'Left' } },
-	    mods = 'CTRL',
-	    action = wezterm.action.OpenLinkAtMouseCursor,
-	  },
-	},
+  color_scheme = 'Gruvbox dark, medium (base16)',
+  colors = {
+    background = '#1d2021',
+  },
+  enable_tab_bar = false,
+  font_size = 16.0,
+  font = wezterm.font('JetBrains Mono', { weight = 'Medium' }),
+  -- font = wezterm.font('GeistMono Nerd Font', { weight = 'Medium' }),
+  -- macos_window_background_blur = 30,
+  -- window_background_opacity = 0.9,
+  window_decorations = 'RESIZE',
+  window_close_confirmation = 'NeverPrompt',
+  window_padding = {
+    -- left = 0,
+    -- right = 0,
+    -- top = 0,
+    bottom = 0,
+  },
+  native_macos_fullscreen_mode = true,
+  mouse_bindings = {
+    {
+      event = { Up = { streak = 1, button = 'Left' } },
+      mods = 'CTRL',
+      action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+  },
 }
