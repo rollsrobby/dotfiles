81595828889f308d5b94cfc1df28f58ea2cd272e
Robbi <30313644+rollsrobby@users.noreply.github.com>
2024-09-19 16:44:03 +0200

    add indentation settings for cs files

--- nvim/.config/nvim/lua/rms/core/autocmds.lua
+++ nvim/.config/nvim/lua/rms/core/autocmds.lua
@@ -10,3 +10,12 @@ api.nvim_create_autocmd(
   { "InsertEnter", "WinLeave" },
   { pattern = "*", command = "set nocursorline", group = cursorGrp }
 )
+api.nvim_create_autocmd("FileType", {
+  pattern = "cs",
+  callback = function()
+    vim.opt_local.tabstop = 4          -- Number of spaces that a <Tab> counts for
+    vim.opt_local.softtabstop = 4      -- Number of spaces that a <Tab> counts for while editing
+    vim.opt_local.shiftwidth = 4       -- Number of spaces to use for each step of (auto)indent
+    vim.opt_local.expandtab = true     -- Convert tabs to spaces
+  end,
+})
