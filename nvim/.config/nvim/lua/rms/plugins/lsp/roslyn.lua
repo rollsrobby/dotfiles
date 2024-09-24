573c2e862a48223f770a0b1cbba4be820469c723
Robbi <30313644+rollsrobby@users.noreply.github.com>
2024-09-24 21:45:50 +0200

    update roslyn keymaps

--- nvim/.config/nvim/lua/rms/plugins/lsp/roslyn.lua
+++ nvim/.config/nvim/lua/rms/plugins/lsp/roslyn.lua
@@ -4,20 +4,23 @@ return {
   opts = {},
   config = function()
     require('roslyn').setup({});
-    local opts = { noremap = true, silent = true }
 
-    vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', opts)
-    vim.keymap.set('n', 'gT', '<cmd>Telescope lsp_type_definitions<CR>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts)
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<leader>D', '<cmd>Telescope diagnostics bufnr=0<CR>', opts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', 'gd', builtin.lsp_definitions, { noremap = true, silent = true, desc = 'Go to definition' })
+    vim.keymap.set('n', 'gT', builtin.lsp_type_definitions,
+      { noremap = true, silent = true, desc = 'Go to type definition' })
+    vim.keymap.set('n', 'gr', builtin.lsp_references, { noremap = true, silent = true, desc = 'List references' })
+    vim.keymap.set('n', 'gi', builtin.lsp_implementations,
+      { noremap = true, silent = true, desc = 'Go to implementations' })
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { noremap = true, silent = true })
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { noremap = true, silent = true })
+    vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, { noremap = true, silent = true })
+    -- vim.keymap.set('n', '<leader>D', builtin.diagnostics({ bufnr = 0 }), { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>D', '<cmd>Telescope diagnostics bufnr=0<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { noremap = true, silent = true })
     vim.keymap.set('n', '<leader>gf', function()
       vim.lsp.buf.format({ async = true })
-    end, opts)
+    end, { noremap = true, silent = true })
   end
 }
