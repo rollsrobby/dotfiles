1dc6eee24fa419cc076d4a9dd1edf4a6864cde5c
Robbi <30313644+rollsrobby@users.noreply.github.com>
2024-11-19 15:36:32 +0100

    remove subfolder

--- nvim/nvim/lua/rms/plugins/netcoredbg.lua
+++
@@ -1,7 +0,0 @@
-return {
-	-- "Cliffback/netcoredbg-macOS-arm64.nvim",
-	-- dependencies = { "mfussenegger/nvim-dap" },
-	-- config = function()
-	-- 	require('netcoredbg-macOS-arm64').setup(require('dap'))
-	-- end
-}
