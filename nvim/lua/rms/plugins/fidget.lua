a72dcc8bd091b3a3df32888ab3ca3ea8553724bc
Robbi <30313644+rollsrobby@users.noreply.github.com>
2024-12-05 17:56:46 +0100

    feat: update fidget

--- nvim/lua/rms/plugins/fidget.lua
+++ nvim/lua/rms/plugins/fidget.lua
@@ -1,14 +1,6 @@
 return {
 	'j-hui/fidget.nvim',
-	tag = 'legacy',
-	event = { 'LspAttach' },
-	opts = {},
 	config = function()
-		require('fidget').setup({
-			window = {
-				blend = 0,
-				relative = 'editor',
-			}
-		})
+		require('fidget').setup()
 	end,
 }
