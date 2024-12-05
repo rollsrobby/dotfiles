ee161b154c8c8c975aa20c72f5484cf3f3d375b1
Robbi <30313644+rollsrobby@users.noreply.github.com>
2024-12-05 17:56:56 +0100

    move out lsp config

---
+++ nvim/lua/rms/plugins/lsp.lua
@@ -0,0 +1,77 @@
+return {
+  'williamboman/mason.nvim',
+  dependencies = {
+  'williamboman/mason-lspconfig.nvim',
+  'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp'
+  },
+  -- event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    require('mason').setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+      border = "rounded",
+    })
+
+    local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
+    for type, icon in pairs(signs) do
+      local hl = 'DiagnosticSign' .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+    end
+
+    local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local on_attach = function(_, bufnr)
+      local opts = { noremap = true, silent = true, buffer = bufnr }
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+      vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', opts)
+      vim.keymap.set('n', 'gT', '<cmd>Telescope lsp_type_definitions<CR>', opts)
+      vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', opts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+      vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, opts)
+      vim.keymap.set('n', '<leader>D', '<cmd>Telescope diagnostics bufnr=0<CR>', opts)
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', '<leader>gf', function()
+        vim.lsp.buf.format({
+          async = true,
+        })
+      end, opts)
+    end
+
+
+    local handlers = {
+      function (server_name)
+        require('lspconfig')[server_name].setup({
+          capabilities = capabilities,
+          on_attach = on_attach
+        })
+      end,
+      ['lua_ls'] = function()
+        local lspconfig = require('lspconfig')
+        lspconfig.lua_ls.setup({
+          settings = { 
+            Lua = {
+              diagnostics = {
+                globals = { 'vim' },
+              },
+            }
+          }
+        });
+      end
+    }
+    require('mason-lspconfig').setup({
+      automatic_installation = true,
+      handlers = handlers
+    })
+
+  end
+}
