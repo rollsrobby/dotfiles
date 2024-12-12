15e1a06e017b69e1880279090827dbfc9419c150
Robbi <30313644+rollsrobby@users.noreply.github.com>
2024-12-12 17:01:17 +0100

    use leader dash

--- nvim/lua/rms/plugins/oil.lua
+++ nvim/lua/rms/plugins/oil.lua
@@ -1,17 +1,17 @@
 return {
-	'stevearc/oil.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	config = function()
-		require('oil').setup({
-			keymaps = {
-				["<C-h>"] = false,
-				["<C-l>"] = false,
-				["<C-r>"] = "actions.refresh",
-				["q"] = function()
-					require('oil').close()
-				end,
-			}
-		})
-		vim.keymap.set('n', '<leader>e', '<CMD>Oil<CR>', {})
-	end
+  'stevearc/oil.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('oil').setup({
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<C-l>"] = false,
+        ["<C-r>"] = "actions.refresh",
+        ["q"] = function()
+          require('oil').close()
+        end,
+      }
+    })
+    vim.keymap.set('n', '<leader>-', '<CMD>Oil<CR>', {})
+  end
 }
