9b34125125168fe3e88243ce488fa93a1dd7b4a5
Robbi <30313644+rollsrobby@users.noreply.github.com>
2024-12-19 09:56:43 +0100

    change shiftwidth for cs files

---
+++ nvim/after/ftplugin/cs.lua
@@ -0,0 +1,6 @@
+local set = vim.opt_local
+
+set.tabstop = 4      -- Number of spaces that a <Tab> counts for
+set.softtabstop = 4  -- Number of spaces that a <Tab> counts for while editing
+set.shiftwidth = 4   -- Number of spaces to use for each step of (auto)indent
+set.expandtab = true -- Convert tabs to spaces
