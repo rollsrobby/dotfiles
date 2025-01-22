6c247c8c1f061f667260dd7b5e7d68b7c791ba57
Robbi <30313644+rollsrobby@users.noreply.github.com>
2025-01-22 09:30:36 +0100

    test replace comments and surround with mini.nvim modules

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    "numToStr/Comment.nvim",
-    config = function()
-      require "Comment".setup()
-    end
-  },
+  -- {
+  --   "numToStr/Comment.nvim",
+  --   config = function()
+  --     require "Comment".setup()
+  --   end
+  -- },
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'echasnovski/mini.nvim',
+    version = false,
+    config = function()
+      require('mini.ai').setup()
+      require('mini.bracketed').setup()
+      require('mini.comment').setup()
+      require('mini.surround').setup()
+    end
+  }
+}
