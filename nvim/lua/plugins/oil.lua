936cc113c2c6f71921b12d85dce13db1d882c009
Robbi <30313644+rollsrobby@users.noreply.github.com>
2025-01-23 12:45:16 +0100

    remap oil keys

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,13 +1,19 @@
 return {
-	{
-		"stevearc/oil.nvim",
-		---@module 'oil',
-		---@type oil.SetupOpts
-		opts = {},
-		depdencies = { { "echasnovski/mini.icons", opts = {} } },
-		config = function()
-			require("oil").setup()
-			vim.keymap.set("n", "-","<cmd>Oil<cr>", { desc = "Open parent directory" })
-		end
-	}
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil',
+    ---@type oil.SetupOpts
+    opts = {},
+    depdencies = { { "echasnovski/mini.icons", opts = {} } },
+    config = function()
+      require("oil").setup({
+        keymaps = {
+          ["<C-h>"] = false,
+          ["<C-l>"] = false,
+          ["<C-r>"] = "actions.refresh",
+        }
+      })
+      vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+    end
+  }
 }
