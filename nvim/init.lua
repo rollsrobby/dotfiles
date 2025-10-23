cf84521eae81b541f901c75108086be7a83eb7aa
Robbi <30313644+rollsrobby@users.noreply.github.com>
2025-10-23 10:30:36 +0200

    replace telescope with snacks picker

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,10 +23,10 @@ capabilities = require('blink.cmp').get_lsp_capabilities(capabilities);
 
 local on_attach = function(_, bufnr)
   local opts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', 'grd', '<cmd>Telescope lsp_definitions theme=ivy<CR>', opts)
-  vim.keymap.set('n', 'grr', '<cmd>Telescope lsp_references theme=ivy<cr>', opts)
-  vim.keymap.set('n', 'gri', '<cmd>Telescope lsp_implementations theme=ivy<CR>', opts)
-  vim.keymap.set('n', '<leader>D', '<cmd>Telescope diagnostics bufnr=0<CR>', opts)
+  vim.keymap.set('n', 'grd', function() Snacks.picker.lsp_definitions() end, opts)
+  vim.keymap.set('n', 'grr', function() Snacks.picker.lsp_references() end, opts)
+  vim.keymap.set('n', 'gri', function() Snacks.picker.lsp_implementations() end, opts)
+  vim.keymap.set('n', '<leader>D', function() Snacks.picker.diagnostics_buffer() end, opts)
   vim.keymap.set('n', 'gra', vim.lsp.buf.code_action, opts)
 end
 
