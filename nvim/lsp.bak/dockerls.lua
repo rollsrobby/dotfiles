9c9814d9922b9483a76ddf1f63a8853f20aeb167
Robbi <30313644+rollsrobby@users.noreply.github.com>
2026-04-08 17:50:15 +0200

    remove old lsp configs

--- nvim/lsp.bak/dockerls.lua
+++
@@ -1,8 +0,0 @@
--- as seen on https://github.com/neovim/nvim-lspconfig/tree/master/lsp
-
-
-return {
-  cmd = { 'docker-langserver', '--stdio' },
-  filetypes = { 'dockerfile' },
-  root_markers = { 'Dockerfile' },
-}
