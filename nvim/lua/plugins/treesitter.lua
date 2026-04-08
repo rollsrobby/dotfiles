46d4d97124bd182a716954bae7bcdb2c722743c9
Robbi <30313644+rollsrobby@users.noreply.github.com>
2026-04-08 17:50:59 +0200

    remove treesiter master config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = { "OXY2DEV/markview.nvim" },
     build = ":TSUpdate",
-    branch = "master",
     lazy = false,
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-        ensure_installed = { "lua", "json", "vim", "vimdoc", "query", "javascript", "html", "typescript", "http", "tsx", "c_sharp", "css", "bash", "dockerfile", "xml", "astro" },
-        sync_install = false,
-        auto_install = false,
-        ignore_install = {},
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
   }
 }
