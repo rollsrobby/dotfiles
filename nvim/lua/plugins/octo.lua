0f3526374e9c9351c398743efe94956ab8f30b01
Robbi <30313644+rollsrobby@users.noreply.github.com>
2026-05-04 14:14:46 +0200

    update octo config

--- nvim/lua/plugins/octo.lua
+++ nvim/lua/plugins/octo.lua
@@ -2,16 +2,21 @@ return {
   "pwntester/octo.nvim",
   cmd = "Octo",
   opts = {
-    -- or "fzf-lua" or "snacks" or "default"
     picker = "snacks",
     -- bare Octo command opens picker of commands
     enable_builtin = true,
   },
+  keys = {
+    {
+      "<leader>O",
+      "<cmd>Octo<cr>",
+      mode = { "n", "x", "v" },
+      desc = "Octo"
+    }
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "folke/snacks.nvim",
-    -- OR "nvim-telescope/telescope.nvim",
-    -- OR "ibhagwan/fzf-lua",
     "nvim-tree/nvim-web-devicons",
   },
 }
