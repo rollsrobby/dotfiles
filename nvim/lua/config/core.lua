54cddcb848361bfdd74334ebf729bf1727240a13
rollsrobby <rms@gitignore.dev>
2026-07-29 10:09:23 +0200

    cleanup

--- nvim/lua/config/core.lua
+++ nvim/lua/config/core.lua
@@ -101,3 +101,15 @@ vim.api.nvim_create_autocmd("FileType", {
     end
   end,
 })
+
+local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = signs.Error,
+      [vim.diagnostic.severity.WARN] = signs.Warn,
+      [vim.diagnostic.severity.HINT] = signs.Hint,
+      [vim.diagnostic.severity.INFO] = signs.Info,
+    }
+  }
+})
