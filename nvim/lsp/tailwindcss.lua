d15617848d97ea6b811dd180faf228858e36db30
rollsrobby <rms@gitignore.dev>
2026-08-22 09:12:03 +0200

    tailwind things

--- nvim/lsp/tailwindcss.lua
+++ nvim/lsp/tailwindcss.lua
@@ -1,15 +1,50 @@
+local stylesheet = 'packages/ui/src/styles/globals.css'
+
 return {
-  settings = {
-    tailwindCSS = {
-      experimental = {
-        configFile = {
-          ["packages/ui/src/styles/globals.css"] = {
-            "packages/ui/src/**",
-            "apps/admin/src/**",
-            "apps/teams-app/src/**",
+  before_init = function(params, config)
+    config.settings = vim.tbl_deep_extend('keep', config.settings or {}, {
+      editor = {
+        tabSize = vim.lsp.util.get_effective_tabstop(),
+      },
+    })
+
+    local root = params.rootUri and vim.uri_to_fname(params.rootUri)
+    if not root then
+      return
+    end
+
+    local stylesheet_path = vim.fs.joinpath(root, stylesheet)
+    if not vim.uv.fs_stat(stylesheet_path) then
+      return
+    end
+
+    config.settings = vim.tbl_deep_extend('force', config.settings, {
+      tailwindCSS = {
+        experimental = {
+          configFile = {
+            [stylesheet] = {
+              'packages/ui/src/**',
+              'apps/admin/src/**',
+              'apps/teams-app/src/**',
+            },
           },
         },
       },
-    },
-  },
+    })
+  end,
 }
+-- return {
+--   settings = {
+--     tailwindCSS = {
+--       experimental = {
+--         configFile = {
+--           ["packages/ui/src/styles/globals.css"] = {
+--             "packages/ui/src/**",
+--             "apps/admin/src/**",
+--             "apps/teams-app/src/**",
+--           },
+--         },
+--       },
+--     },
+--   },
+-- }
